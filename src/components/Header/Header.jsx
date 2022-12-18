84bb483779095a6e0f650967b314838d0456577c
Ejim Favour <favourejim56@gmail.com>
2022-12-18 22:27:00 +0100

    feat: added the header content

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,12 @@
 import classes from "./Header.module.scss";
+import logo from "../../asset/Logo.svg";
 
 const Header = () => {
-  return <header></header>;
+  return (
+    <header className={classes.header}>
+      <img src={logo} alt="logo" />
+    </header>
+  );
 };
 
 export default Header;
