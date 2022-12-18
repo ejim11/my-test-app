0ec8326056af65b1df55703b269d5cd461e7dffd
Ejim Favour <favourejim56@gmail.com>
2022-12-18 22:15:55 +0100

    feat: created the layout

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,13 @@
+import { Main } from "next/document";
+import Header from "../Header/Header";
+
+const Layout = () => {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
+};
+
+export default Layout;
