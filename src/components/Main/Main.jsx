027d680c4ebac2eddfe2342dd0389b1f7064b82f
Ejim Favour <favourejim56@gmail.com>
2022-12-18 22:53:30 +0100

    feat: added the first section

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,5 +1,16 @@
+import classes from "./Main.module.scss";
+
 const Main = () => {
-  return <main></main>;
+  return (
+    <main>
+      <section className={classes["first-section"]}>
+        <h2>
+          Watch
+          <br /> something <br /> incredible.
+        </h2>
+      </section>
+    </main>
+  );
 };
 
 export default Main;
